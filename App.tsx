1187087aa5c1d576469c4349b9bfd67fab058242
Marianasantos142 <111981338+Marianasantos142@users.noreply.github.com>
2022-12-08 10:28:52 -0300

    Add files via upload

--- App.tsx
+++ App.tsx
@@ -1,39 +1,60 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/estaticos/navbar/Navbar';
+import Footer from './components/estaticos/footer/Footer';
+import CadastroUsuario from './paginas/cadastroUsuario/CadastroUsuario';
 import Home from './paginas/home/Home';
-import { Grid } from '@material-ui/core';
-import Navbar from './componetes/estaticos/navbar/Navbar';
-import Footer from './componetes/estaticos/footer/Footer';
-import Login from './paginas/Login/Login';
+import Login from './paginas/login/Login';
+import './App.css';
+import ListaTema from './components/temas/listatema/ListaTema';
+import ListaPostagem from './components/postagens/listapostagem/ListaPostagem';
+import CadastroPost from './components/postagens/cadastroPost/CadastroPost';
+import CadastroTema from './components/temas/cadastroTema/CadastroTema';
+import DeletarPostagem from './components/postagens/deletarPostagem/DeletarPostagem';
+import DeletarTema from './components/temas/deletarTema/DeletarTema';
+import { Provider } from 'react-redux';
+import store from './store/store';
 
 
 function App() {
   return (
-
-    <>
+    <Provider store={store}>
       <Router>
         <Navbar />
-        <Routes>
+       
+          <div style={{ minHeight: '100vh' }}>
+          <Routes>
 
+            <Route path="/" element={<Login />} />
 
-          <Route path='/home' element={<Home />} />
+            <Route path="/login" element={<Login />} />
 
-          <Route path='/login' element={<Login />} />
+            <Route path="/home" element={<Home />} />
 
-        </Routes>
+            <Route path="/cadastrousuario" element={<CadastroUsuario />} />
+
+            <Route path="/temas" element={<ListaTema />} />
+
+            <Route path="/posts" element={<ListaPostagem />} />
+
+            <Route path="/formularioPostagem" element={<CadastroPost />} />
+
+            <Route path="/formularioPostagem/:id" element={<CadastroPost />} />
+
+            <Route path="/formularioTema" element={<CadastroTema />} />
+
+            <Route path="/formularioTema/:id" element={<CadastroTema />} />
+
+            <Route path="/deletarPostagem/:id" element={<DeletarPostagem />} />
+
+            <Route path="/deletarTema/:id" element={<DeletarTema />} />
+            </Routes>
+          </div>
+      
         <Footer />
-
       </Router>
-
-
-
-
-
-
-
-    </>
+    </Provider>
   );
 }
+
 export default App;
